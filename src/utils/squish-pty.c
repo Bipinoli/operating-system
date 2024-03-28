c60e80472cc8bbc650aa0613f8d88ac7328545b0
bipinoli <bipinoli@bipin.myguest.virtualbox.org>
2024-03-28 19:36:58 +0100

    stropts.h bug fix + generated docs

--- src/utils/squish-pty.c
+++ src/utils/squish-pty.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stropts.h>
+//#include <stropts.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -285,12 +285,20 @@ main (int argc __attribute__ ((unused)), char *argv[])
 
   /* System V implementations need STREAMS configuration for the
      slave. */
+  /*
+   * Changed by Bipin:
+   * stropts.h is no longer included in libc
+   * so isastream & I_PUSH are not available
+   * so commenting the code below
+   */
+  /*
   if (isastream (slave))
     {
       if (ioctl (slave, I_PUSH, "ptem") < 0
           || ioctl (slave, I_PUSH, "ldterm") < 0)
         fail_io ("ioctl");
     }
+    */
 
   /* Arrange to get notified when a child dies, by writing a byte
      to a pipe fd.  We really want to use pselect() and
